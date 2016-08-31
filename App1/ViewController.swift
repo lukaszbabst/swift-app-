9f4c748498cb0d95a33fadb09f296d000c2067f4
Łukasz Babst <lukaszbabst@ukaszs-macbook-pro.local>
2016-08-31 21:46:26 +0200

    Initial Commit

---
+++ App1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App1
+//
+//  Created by Łukasz Babst on 31/08/2016.
+//  Copyright © 2016 Łukasz Babst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
